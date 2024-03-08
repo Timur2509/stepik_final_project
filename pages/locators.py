96316942c8ab5e7cc719657ed189c24ca3e892c2
My Name <captimlost25@gmail.com>
2024-03-08 13:31:45 +0300

    Поиск бага через параметризацию и его выделение xfail

--- pages/locators.py
+++ pages/locators.py
@@ -10,3 +10,13 @@ class LoginPageLocators():
     PASSWORD_FIELD = (By.CSS_SELECTOR, "#registration-password1")
     PASSWORD_CHECK = (By.CSS_SELECTOR, "#registration-password2")
     SUBMIT_BUTTON = (By.NAME,"registration_submit")
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    CHECK_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    CHECK_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    NAME_AFTER_ADD_ITEM = (By.CSS_SELECTOR,".alert-success:first-child .alertinner strong")
+    PRICE_AFTER_ADD_ITEM = (By.CSS_SELECTOR,".alert-info .alertinner strong")
+    WHOLE_SUCCESS_MESSAGE = (By.CSS_SELECTOR,".alert-safe.alert-noicon")
+
+
---
+++ pages/product_page.py
@@ -0,0 +1,31 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_item_to_basket(self):
+        #нажимаем на кнопку "Добавить товар в корзину"
+        add_button = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
+        add_button.click()
+    
+    def there_are_basket_button(self):
+        assert self.is_element_is_present( *ProductPageLocators.BUTTON_ADD_TO_BASKET), "Нет кнопки добавления товара"
+    
+    def there_are_product_name(self):
+        assert self.is_element_present(*ProductPageLocators.CHECK_NAME), "Отсутствует название продукта"
+    
+    def there_are_product_price(self):
+        assert self.is_element_present(*ProductPageLocators.CHECK_PRICE), "Нет цены продукта"
+    
+    def item_name_eql_basket_name(self):
+        name_of_item = self.browser.find_element(*ProductPageLocators.CHECK_NAME).text
+        name_of_item_after = self.browser.find_element(*ProductPageLocators.NAME_AFTER_ADD_ITEM).text
+        assert name_of_item == name_of_item_after, "Название книги в корзине не совпадает с оригинальным"
+
+    def item_price_eql_basket_price(self):
+        price_of_item = self.browser.find_element(*ProductPageLocators.CHECK_PRICE).text
+        price_of_item_after = self.browser.find_element(*ProductPageLocators.PRICE_AFTER_ADD_ITEM).text
+        assert price_of_item_after ==  price_of_item, "Цена книги в корзине совпадает с оригинальной"
+
+    def success_message_appeared(self):
+        assert self.is_element_present(*ProductPageLocators.WHOLE_SUCCESS_MESSAGE)
+
